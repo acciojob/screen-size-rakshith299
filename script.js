1cd992c8d46f9d5c5d2cf6260348b8fb08984c16
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-17 17:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 
 window.addEventListener("resize", function(){
-    let height = this.window.innerHeight;
-    let width = this.window.innerWidth;
+    let height = window.innerHeight;
+    let width = window.innerWidth;
 
     let h1ele = document.querySelector("#sizeInfo > h1");
 
 
-    h1ele.innerHTML = `Width: ${height} and Height: ${width}`;
+    h1ele.innerHTML = `Width: ${width} and Height: ${height}`;
 
     
 })
